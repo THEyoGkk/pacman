ae325233f08dbe85081c729b7fbb4c51903b153f
THEyoGkk <e.jack.zsd@gmail.com>
2015-06-25 01:30:10 +0300

    map,coins,pacman,movement

---
+++ move.js
@@ -0,0 +1,91 @@
+var pacman = null;
+var coinsLayer;
+var pacmanLayer;
+var socket = new WebSocket("ws://localhost:1337");
+
+socket.onmessage = function (event) {
+    var box = JSON.parse(event.data);
+    switch (box.pacman) {
+        case "hello!":
+            pacman = new KeyBox();  
+            break; 
+        case "pacmanMovement":        
+            pacman.move(parseInt(box.left,10),parseInt(box.top,10));
+            break;
+        case "pacmanCoinsMap":
+            drawCoinsMap(parseInt(box.x,10),parseInt(box.y,10));
+            break;
+        case "pacmanImg":
+            drawPacman(parseInt(box.x,10),parseInt(box.y,10));
+            break;
+        default:
+            break;
+    }
+};
+
+
+
+
+function InitBox(){
+	document.getElementById("b").addEventListener("keydown",pacmanMove);
+    socket.send(JSON.stringify({method:"init"}));
+    return;
+}
+
+function pacmanMove(e){ 
+    console.log(e);  
+    socket.send(JSON.stringify({method:"pacman",keyCode:e.keyCode.toString()}));
+}
+
+function KeyBox(){
+	this.node = document.getElementById("square");
+	this.left = 0;
+    this.top = 0;
+    this.width = this.node.clientWidth;
+    this.height = this.node.clientHeight;
+    this.dx = 16;
+    this.dy = 16;
+}
+
+KeyBox.prototype.move = function(dx,dy) {
+    pacmanLayer = document.getElementById("pacman").getContext('2d');
+    pacmanLayer.clearRect(0, 0, document.getElementById("pacman").width, document.getElementById("pacman").height);
+    drawPacman(dx,dy);	
+};
+
+
+
+function draw(){
+        var canvas = document.getElementById('map');
+        var mapLayer = canvas.getContext('2d'); 
+        base_image = new Image();
+        base_image.src = 'img/map.jpg';
+        base_image.onload = function(){
+        mapLayer.drawImage(base_image, -4, -5, 448 + 8, 496 + 8);
+        coinsLayer = document.getElementById("coins").getContext('2d');
+        pacmanLayer = document.getElementById("pacman").getContext('2d');
+       
+   
+        //drawBorders(coinsLayer,1,1,448,576,8,16);
+        
+    
+  }
+}
+
+function drawCoinsMap(x,y) {
+    drawCoin(coinsLayer,x,y);
+}
+
+function drawCoin(ctx,x,y) {
+    ctx.fillStyle = '#ff0';
+    ctx.fillRect(x,y,4,4);
+}
+
+function drawPacman(x,y) {   
+        pacmanLayer.beginPath();
+        pacmanLayer.arc(x, y, 8,Math.PI/7,-Math.PI/7,false);
+        pacmanLayer.lineTo(x - 2,y + 1);
+        pacmanLayer.lineTo(x + 7,y + 4);
+        pacmanLayer.fillStyle = "#ff0";
+        pacmanLayer.fill();    
+}
---
+++ server.js
@@ -0,0 +1,158 @@
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 1337
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("\nhttp server listening on %d", port)
+
+var wss = new WebSocketServer({ server: server })
+console.log("\nwebsocket server created")
+
+
+var coinsMap = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+       [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
+       [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+       [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+       [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+       [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+       [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
+       [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
+       [0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0],
+       [0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,1,0,0,0,2,2,0,0,0,1,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,1,1,1,0,0,2,2,2,2,0,0,1,1,1,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,1,0,0,2,2,2,2,0,0,1,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+       [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+       [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
+       [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+       [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+       [0,1,1,1,0,0,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,0,0,1,1,1,0],
+       [0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0],
+       [0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0],
+       [0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0],
+       [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0],
+       [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0],
+       [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+       [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
+
+
+/*
+Game initializing
+*/
+
+function gameInit(ws){
+	console.log("\nmainGameInit...")
+	pacmanInit(ws);
+}
+
+function drawCoinsMap(ws){
+	for (var i = 0; i < 28; i++) {
+		for(var j = 0; j < 31; j ++){
+			if (coinsMap[j][i] === 1) {
+				ws.send(JSON.stringify({pacman:"pacmanCoinsMap",x: (16 * (i + 0.5)).toString(), y: (16 * (j + 0.5)).toString() }))
+			}
+		}
+	}
+}
+
+
+
+
+function pacmanInit(ws){
+	console.log("\npacmanInit...")
+	pacman = new Pacman();
+	ws.send(JSON.stringify({pacman:"hello!"}));
+}
+
+/*######################################*/
+
+
+/*
+Pacman 
+*/
+
+var pacman = null;
+
+function Pacman(){
+	this.x = 13.5;
+	this.y = 23.5;
+}
+
+function drawPacmanImg(ws) {
+	ws.send(JSON.stringify({pacman:"pacmanImg",x:((pacman.x)  * 16).toString()
+											  ,y:((pacman.y) * 16).toString()}));
+}
+
+function pacmanMove(keyCode,ws){
+	console.log("move");
+	if(keyCode == 37){
+		ws.send(JSON.stringify({pacman:"pacmanMovement"
+							   ,left:(pacman.x * 16 - 16).toString()
+							   ,top:((pacman.y) * 16 + 0).toString()}));
+		pacman.x -= 1;		
+		return;
+	}else if(keyCode == 38){
+		ws.send(JSON.stringify({pacman:"pacmanMovement"
+							   ,left:(pacman.x * 16).toString()
+							   ,top:((pacman.y) * 16 - 16).toString()}));
+		pacman.y -= 1;	
+		return;
+	}else if(keyCode == 39){
+		ws.send(JSON.stringify({pacman:"pacmanMovement"
+							   ,left:(pacman.x * 16 + 16).toString()
+							   ,top:((pacman.y) * 16 + 0).toString()}));
+		pacman.x += 1;	
+		return;
+	}else if(keyCode == 40){
+		ws.send(JSON.stringify({pacman:"pacmanMovement"
+							   ,left:(pacman.x * 16).toString()
+							   ,top:((pacman.y) * 16 + 16).toString()}));
+		pacman.y += 1;	
+		return;
+	}
+	return;
+}
+
+
+
+
+
+/*######################################*/
+
+
+wss.on("connection", function (ws) {
+    ws.on("message", function (message) {
+        var clientMessage = JSON.parse(message);
+        switch(clientMessage.method){
+        	case "init":
+        		gameInit(ws);
+				drawPacmanImg(ws);
+				drawCoinsMap(ws);				
+        		break;
+        	case "pacman":
+        		pacmanMove(clientMessage.keyCode,ws);
+        		break;
+        	default:
+        		break;
+        }     
+                     
+    })
+    
+    ws.on("close", function () {
+       
+    })
+
+});
+
